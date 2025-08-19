f4fb6992379e5df594250cdff2766e631ab6f74c
Nikhil kumar <nikhilkumar14082005@gmail.com>
2025-08-19 13:33:56 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import Myform from './components/Myform'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+<Myform/>
+
+
+      </div>
+    </>
+  )
+}
+
+export default App
